6e5f0565710c4b85d6ab06eba5f003e3494481f5
Shirish Koirala <shirishkoirala@gmail.com>
2024-08-26 17:28:11 +1000

    Initial Commit

---
+++ SplashReel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplashReel
+//
+//  Created by Shirish Koirala on 26/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
